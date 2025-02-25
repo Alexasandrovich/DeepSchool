01efc046ad38d9abb3016e390264af4c12d64eea
Aleksandr Arsentev <aleksandrars@hflabs.ru>
2025-02-25 14:48:19 +0300

    fixes

--- speed_up_nn/hw_02/utils/train.py
+++ speed_up_nn/hw_02/utils/train.py
@@ -6,6 +6,7 @@ from torch import nn
 from tqdm.auto import tqdm
 from torch.nn import functional as F
 from .model import evaluate_model
+from transformers import get_linear_schedule_with_warmup
 
 @dataclass
 class TrainParams:
@@ -20,6 +21,7 @@ class TrainParams:
     last_layer_loss_weight: float
     intermediate_attn_layers_weights: tp.Tuple[float, float, float, float]
     intermediate_feat_layers_weights: tp.Tuple[float, float, float, float]
+    warmup_steps: int
 
 
 mse_loss = nn.MSELoss()
@@ -30,8 +32,31 @@ id2label = {
 }
 
 
+class TeacherToStudentAdapter(nn.Module):
+    # we make an adapter from teacher to student, as it is easier to throw away the excess than to invent something else
+    def __init__(self, teacher_dim: int, student_dim: int):
+        super().__init__()
+        self.adapter = nn.Sequential(
+            nn.Linear(teacher_dim, student_dim),
+            nn.ReLU()
+        )
+
+    def forward(self, teacher_features: torch.Tensor) -> torch.Tensor:
+        batch, channels, h, w = teacher_features.shape
+        teacher_features = teacher_features.permute(0,2,3,1).contiguous()  # [B, H, W, C]
+        teacher_features = teacher_features.view(-1, channels)  # [B*H*W, C]
+        adapted = self.adapter(teacher_features)  # [B*H*W, student_dim]
+        adapted = adapted.view(batch, h, w, -1).permute(0, 3, 1, 2).contiguous()
+        return adapted
+
+adapters = nn.ModuleList([
+    TeacherToStudentAdapter(teacher_dim=32, student_dim=16),
+    TeacherToStudentAdapter(teacher_dim=64, student_dim=32),
+    TeacherToStudentAdapter(teacher_dim=160, student_dim=80),
+    TeacherToStudentAdapter(teacher_dim=256, student_dim=128)
+]).to('cuda')
+
 def calc_last_layer_loss(student_logits, teacher_logits, weight, temperature=1.0):
-    """Считаем лосс между выходами учителя и ученика"""
     student_log_probs = F.log_softmax(student_logits / temperature, dim=-1)
     teacher_probs = F.softmax(teacher_logits / temperature, dim=-1)
 
@@ -42,9 +67,10 @@ def calc_intermediate_layers_feat_loss(student_feats, teacher_feats, weights):
     total_loss = 0.0
     for i, student_feat in enumerate(student_feats):
         teacher_feat = teacher_feats[i]
-        # probably need train adapter for adapter(teacher_feat) mapping with same shapes as student
 
-        loss = mse_loss(student_feat, teacher_feat)
+        # need adapter after pruning
+        adapted_teacher_feat = adapters[i](teacher_feat)
+        loss = mse_loss(student_feat, adapted_teacher_feat)
         weight_value = weights[i] if i < len(weights) else 1.0
 
         total_loss += weight_value * loss
@@ -85,7 +111,17 @@ def train(
         num_workers=train_params.n_workers,
     )
 
-    optimizer = torch.optim.AdamW(student_model.parameters(), lr=train_params.lr)
+    optimizer = torch.optim.AdamW(
+        list(student_model.parameters()) + list(adapters.parameters()),
+        lr=train_params.lr
+    )
+    # warmup need after pruning
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=train_params.warmup_steps,
+        num_training_steps=len(train_dataloader) * train_params.n_epochs
+    )
+
     step = 0
     for epoch in range(train_params.n_epochs):
         pbar = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
@@ -150,6 +186,7 @@ def train(
 
             total_loss.backward()
             optimizer.step()
+            scheduler.step()
             pbar.set_description(f'total loss: {total_loss.item():.3f}')
 
             for loss_value, loss_name in (
